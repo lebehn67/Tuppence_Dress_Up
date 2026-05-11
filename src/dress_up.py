df9acfa9af3047c0feb9d718f1c8417f781a7858
silly goose <lebehnken@outlook.com>
2026-05-11 00:33:03 -0500

    main was doing way too much so i broke it all up into smaller and simpler funtions

--- src/dress_up.py
+++ src/dress_up.py
@@ -1,6 +1,60 @@
 import pygame
 
+
+# LOAD IMAGES
+def load_images():
+
+    body = pygame.image.load("images/body.png").convert_alpha()
+
+    shirt1 = pygame.image.load("images/shirt1.png").convert_alpha()
+
+    return body, shirt1
+
+
+# HANDLE EVENTS
+def handle_events(shirt_button, shirt1, current_shirt):
+
+    running = True
+
+    for event in pygame.event.get():
+
+        # CLOSE WINDOW
+        if event.type == pygame.QUIT:
+            running = False
+
+        # MOUSE CLICK
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if shirt_button.collidepoint(event.pos):
+
+                current_shirt = shirt1
+
+    return running, current_shirt
+
+
+# DRAW EVERYTHING
+def draw_game(screen, body, current_shirt, shirt_button):
+
+    # BACKGROUND
+    screen.fill((255, 200, 200))
+
+    # BODY
+    screen.blit(body, (250, 100))
+
+    # SHIRT
+    if current_shirt:
+        screen.blit(current_shirt, (250, 100))
+
+    # BUTTON
+    pygame.draw.rect(screen, (0, 0, 255), shirt_button)
+
+    # UPDATE SCREEN
+    pygame.display.update()
+
+
+# MAIN
 def main():
+
     pygame.init()
 
     screen = pygame.display.set_mode((800, 600))
@@ -8,39 +62,38 @@ def main():
 
     clock = pygame.time.Clock()
 
-    body = pygame.image.load("images/base.png")
-    dress1 = pygame.image.load("images/Dress1.png")
+    # LOAD IMAGES
+    body, shirt1 = load_images()
 
-    current_dress = None
+    # CURRENT CLOTHES
+    current_shirt = None
 
-    dress_button = pygame.Rect(50, 50, 100, 50)
+    # BUTTONS
+    shirt_button = pygame.Rect(50, 50, 100, 50)
 
     running = True
 
     while running:
 
-        for event in pygame.event.get():
+        # HANDLE INPUT
+        running, current_shirt = handle_events(
+            shirt_button,
+            shirt1,
+            current_shirt
+        )
 
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if dress_button.collidepoint(event.pos):
-                    current_dress = dress1
-
-
-        screen.fill((255, 200, 200))
-
-        screen.blit(body, (250, 100))
-        if current_dress:
-            screen.blit(dress1, (250, 100))
-
-        pygame.draw.rect(screen, (0, 0, 255), dress_button)
-
-        pygame.display.update()
+        # DRAW FRAME
+        draw_game(
+            screen,
+            body,
+            current_shirt,
+            shirt_button
+        )
 
         clock.tick(60)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
