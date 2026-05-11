7e7d91e7662398c4fa77303d0d3de5d5916e78cf
silly goose <lebehnken@outlook.com>
2026-05-11 00:29:43 -0500

    add most of the images make basis of game with one buttton for a dress

--- src/dress_up.py
+++ src/dress_up.py
@@ -3,32 +3,41 @@ import pygame
 def main():
     pygame.init()
 
-    # Create window
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Tuppence Dress Up")
 
-    # Controls how fast the game runs
     clock = pygame.time.Clock()
 
-    # Game loop
+    body = pygame.image.load("images/base.png")
+    dress1 = pygame.image.load("images/Dress1.png")
+
+    current_dress = None
+
+    dress_button = pygame.Rect(50, 50, 100, 50)
+
     running = True
 
     while running:
 
-        # Check events
         for event in pygame.event.get():
 
-            # If player closes window
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if dress_button.collidepoint(event.pos):
+                    current_dress = dress1
 
-        # Fill background color
+
         screen.fill((255, 200, 200))
 
-        # Update screen
+        screen.blit(body, (250, 100))
+        if current_dress:
+            screen.blit(dress1, (250, 100))
+
+        pygame.draw.rect(screen, (0, 0, 255), dress_button)
+
         pygame.display.update()
 
-        # 60 frames per second
         clock.tick(60)
 
     pygame.quit()
