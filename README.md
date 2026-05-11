7d5eaf4d4267fd01b983573084333c1054fd2b12
silly goose <lebehnken@outlook.com>
2026-05-10 23:47:27 -0500

    more complex main function

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tuppence_Dress_Up
+
+This is a super fun game that helps me visualize different outfits on my character. 
+This game could be modified for any character and will be very useful to me going foreward. 
+I used a tutorial to help with the set up of this game the link is below.
+https://mikomikisomi.weebly.com/current-projects/how-to-easily-make-a-dress-up-game-in-renpy
--- src/dress_up.py
+++ src/dress_up.py
@@ -2,11 +2,36 @@ import pygame
 
 def main():
     pygame.init()
-    pygame.display.set_caption("Tuppence Dress Up")
-    resolution = (800, 600)
-    screen = pygame.display.set_mode(resolution)
-    pygame.quit
 
+    # Create window
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Tuppence Dress Up")
+
+    # Controls how fast the game runs
+    clock = pygame.time.Clock()
+
+    # Game loop
+    running = True
+
+    while running:
+
+        # Check events
+        for event in pygame.event.get():
+
+            # If player closes window
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill background color
+        screen.fill((255, 200, 200))
+
+        # Update screen
+        pygame.display.update()
+
+        # 60 frames per second
+        clock.tick(60)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
