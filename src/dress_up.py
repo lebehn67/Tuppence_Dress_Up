3d9cf12f02ad093c2ea0b8bd7d079f1accbff3be
silly goose <lebehnken@outlook.com>
2026-05-10 23:28:20 -0500

    basic basic main and calling main set up

---
+++ src/dress_up.py
@@ -0,0 +1,12 @@
+import pygame
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Tuppence Dress Up")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    pygame.quit
+
+
+if __name__ == "__main__":
+    main()
